0055f4606e791ea3ac973e9f23d0ccc1ec91021a
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2023-10-03 00:54:22 +0200

    added double jump and wall jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var MAX_HEALTH = 10
 
 var health
+var air_jump
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,6 +15,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	health = MAX_HEALTH
+	air_jump = false
 
 
 func _process(delta):
@@ -33,8 +35,9 @@ func handle_anim(input_axis):
 
 
 func _physics_process(delta):
-	# Handle Jump.
+	handle_wall_jump()
 	handle_jump()
+	
 	apply_gravity(delta)
 
 	var dir = Input.get_axis("move_left", "move_right")
@@ -47,13 +50,29 @@ func _physics_process(delta):
 	if was_on_floor and not is_on_floor() and self.velocity.y >= 0:
 		coyote_jump_timer.start()
 
+
 func handle_jump():
+	if is_on_floor(): air_jump = true
+	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("jump"):
 			self.velocity.y = movement_data.jump_velocity
 	if not is_on_floor():
 		if Input.is_action_just_released("jump") and self.velocity.y < movement_data.jump_velocity / 2:
 			self.velocity.y = movement_data.jump_velocity / 2
+			
+		if Input.is_action_just_pressed("jump") and air_jump:
+			self.velocity.y = movement_data.air_jump_velocity
+			air_jump = false
+
+
+func handle_wall_jump():
+	if not is_on_wall() or is_on_floor(): return
+	
+	var wall_normal = get_wall_normal()
+	if Input.is_action_just_pressed("move_left") and wall_normal == Vector2.LEFT or Input.is_action_just_pressed("move_right") and wall_normal == Vector2.RIGHT:
+		self.velocity.x = movement_data.speed * wall_normal.x
+		self.velocity.y = movement_data.wall_jump_velocity
 
 
 func apply_gravity(delta):
@@ -63,7 +82,11 @@ func apply_gravity(delta):
 
 func handle_horizontal_acceleration(delta, input_axis):
 	if input_axis:
-		self.velocity.x = move_toward(self.velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
+		var target = movement_data.speed * input_axis
+		var acceleration = movement_data.acceleration * delta
+		if not is_on_floor():
+			acceleration *= movement_data.air_accel_scale
+		self.velocity.x = move_toward(self.velocity.x, target, acceleration)
 
 
 func apply_friction(delta, input_axis):
--- scripts/player_movement_data.gd
+++ scripts/player_movement_data.gd
@@ -3,7 +3,10 @@ extends Resource
 
 @export var speed = 150.0
 @export var acceleration = 1000.0
+@export var air_accel_scale = 0.5
 @export var friction = 1200.0
-@export var air_resistance = 200.0
+@export var air_resistance = 100.0
 @export var jump_velocity = -300.0
+@export var air_jump_velocity = -250.0
+@export var wall_jump_velocity = -250.0
 @export var gravity_scale = 1.0
